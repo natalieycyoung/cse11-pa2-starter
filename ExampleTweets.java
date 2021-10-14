4aacc43a715f41b362481ecc72b1156f04e8cc5e
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2021-10-14 01:55:49 -0700

    Complete except checkoff by TA

--- ExampleTweets.java
+++ ExampleTweets.java
@@ -19,69 +19,94 @@ class ExampleTweets
 		System.out.println("\ntestLink: " + tweetTest.toLink());
 		*/
 
-		// User objects created and User toText() method called
+		// USER OBJECTS
 		User user1 = new User("jack", "jack", 5700000);
 		String user1Text = user1.toText();
-//		System.out.println("\nUser 1: " + user1Text);	// User 1: jack @jack		
+		// System.out.println("\nUser 1: " + user1Text);
+		//		User 1: jack @jack		
 
 		User user2 = new User("hodgesboi15", "september22", 1156);
 		String user2Text = user2.toText();
-//		System.out.println("\nUser 2: " + user2Text);	// User 2: september22 @hodgesboi15
+		// System.out.println("\nUser 2: " + user2Text);
+		//		User 2: september22 @hodgesboi15
 	
 		User user3 = new User("ambernoelle", "Night of the Living Discourse", 36700);
 		String user3Text = user3.toText();
-//		System.out.println("\nUser 3: " + user3Text);	// User 3: Night of the Living Discourse @ambernoelle		
+		// System.out.println("\nUser 3: " + user3Text);
+		//		User 3: Night of the Living Discourse @ambernoelle		
 
+		// TWEET OBJECTS
 		// Tweet 1
 		// URL: https://twitter.com/jack/status/20/
 		// Parts of Tweet that couldn't be represented with class design -- no
 		Tweet tweet1 = new Tweet("just setting up my twttr", user1, 170600, "20");
 		String tweet1Text = tweet1.toText();
 		String url1 = tweet1.toLink();
-		//		System.out.println("\nTweet 1: " + tweet1Text);
-		//			Tweet 1: jack @jack : just setting up my twttr : 170600 Likes
-		//		System.out.println("Link: " + url1);
-		//			Link: https://twitter.com/jack/status/20
+		// System.out.println("\nTweet 1: " + tweet1Text);
+		//		Tweet 1: jack @jack : just setting up my twttr : 170600 Likes
+		// System.out.println("Link: " + url1);
+		//		Link: https://twitter.com/jack/status/20
 	
 		// Tweet 2
 		// URL: https://twitter.com/hodgesboi15/status/388859485974171649
 		// Parts of Tweet that couldn't be represented with class design -- no
 		Tweet tweet2 = new Tweet("If I pay $40 for a haunted house I better die", user2, 218000, "388859485974171649");
 		String tweet2Text = tweet2.toText();
-		//		System.out.println("\nTweet 2: " + tweet2Text);
-		//			Tweet 2: september22 @hodgesboi15 : If I pay $40 for a haunted house
-		//			I better die : 218000 Likes
-		//		System.out.println("Link: " + tweet2.toLink());
-		//			Link: https://twitter.com/hodgesboi15/status/388859485974171649
+		String url2 = tweet2.toLink();
+		// System.out.println("\nTweet 2: " + tweet2Text);
+		//		Tweet 2: september22 @hodgesboi15 : If I pay $40 for a haunted house
+		//		I better die : 218000 Likes
+		// System.out.println("Link: " + url2);
+		//		Link: https://twitter.com/hodgesboi15/status/388859485974171649
 		
 		// Tweet 3
 		// URL: https://twitter.com/ambernoelle/status/1440853238606012422
 		// Parts of Tweet that couldn't be represented with class design -- no
 		Tweet tweet3 = new Tweet("I have just discovered that the collective noun for mothers is a consternation, and, well", user3, 286, "1440853238606012422");
 		String tweet3Text = tweet3.toText();
-		//		System.out.println("\nTweet 3: " + tweet3Text);
-		//			Tweet 3: Night of the Living Discourse @ambernoelle : I have just
-		//			discovered that the collective noun for mothers is a consternation,
-		//			and, well : 286 Likes	
-		//		System.out.println("Link: " + tweet3.toLink());
-		//			Link: https://twitter.com/ambernoelle/status/1440853238606012422
+		String url3 = tweet3.toLink();
+		// System.out.println("\nTweet 3: " + tweet3Text);
+		//		Tweet 3: Night of the Living Discourse @ambernoelle : I have just
+		//		discovered that the collective noun for mothers is a consternation,
+		//		and, well : 286 Likes	
+		// System.out.println("Link: " + url3);
+		//		Link: https://twitter.com/ambernoelle/status/1440853238606012422
 
 		// Tweet 4
 		// URL: https://twitter.com/ambernoelle/status/1430195040261971968
 		// Parts of Tweet that couldn't be represented with class design -- no
 		Tweet tweet4 = new Tweet("A haunted house but every room is haunted by a different kind of disappointment", user3, 289, "1430195040261971968");
 		String tweet4Text = tweet4.toText();
-		//		System.out.println("\nTweet 4: " + tweet4Text);
-		//			Tweet 4: Night of the Living Discourse @ambernoelle : A haunted house
-		//			but every room is haunted by a different kind of disappointment : 289
-		//			Likes
-		//		System.out.println("Link: " + tweet4.toLink());
-		//			Link: https://twitter.com/ambernoelle/status/1430195040261971968
+		String url4 = tweet4.toLink();
+		// System.out.println("\nTweet 4: " + tweet4Text);
+		//		Tweet 4: Night of the Living Discourse @ambernoelle : A haunted house
+		//		but every room is haunted by a different kind of disappointment : 289
+		//		Likes
+		// System.out.println("Link: " + url4);
+		//		Link: https://twitter.com/ambernoelle/status/1430195040261971968
 
 
-		// Method Examples
-//		System.out.println("\nlongerThan test: " + tweet1.longerThan(tweetTest));
-//		System.out.println("\nmoreLikes test: " + tweet1.moreLikes(tweetTest));
+		// METHOD EXAMPLES
+		
+		// longerThan()
+		System.out.println("\nTweet 1 longerThan Tweet 2: " + tweet1.longerThan(tweet2));
+			// Tweet 1 longerThan Tweet 2: false
+		System.out.println("Tweet 2 longerThan Tweet 1: " + tweet2.longerThan(tweet1));
+			// Tweet 2 longerThan Tweet 1: true
+		System.out.println("\nTweet 2 longerThan Tweet 3: " + tweet2.longerThan(tweet3));
+			// Tweet 2 longerThan Tweet 3: false
+		System.out.println("Tweet 3 longerThan Tweet 2: " + tweet3.longerThan(tweet2));
+			// Tweet 3 longerThan Tweet 2: true
+
+		// moreLikes()
+		System.out.println("\nTweet 1 moreLikes than Tweet 2: " + tweet1.moreLikes(tweet2));
+			// Tweet 1 moreLikes than Tweet 2: false
+		System.out.println("Tweet 2 moreLikes than Tweet 1: " + tweet2.moreLikes(tweet1));
+			// Tweet 2 moreLikes than Tweet 1: true
+		System.out.println("\nTweet 2 moreLikes than Tweet 3: " + tweet2.moreLikes(tweet3));
+			// Tweet 2 moreLikes than Tweet 3: true
+		System.out.println("Tweet 3 moreLikes than Tweet 2: " + tweet3.moreLikes(tweet2));
+			// Tweet 3 moreLikes than Tweet 2: false
 	}
 }
 
@@ -196,7 +221,6 @@ class Tweet
 	 */
 	String toLink()
 	{
-		//TODO: return string representing URL for Tweet object
 		String objectURL = "https://twitter.com/" + author.userName + "/status/" + uniqueID;
 
 		return objectURL;
